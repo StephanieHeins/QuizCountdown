f6b14c946b492ba9f7657cfdb498bef47bb18af6
Steph Heins <stephanie_heins@yahoo.com>
2021-07-22 13:38:55 -0700

    Changed a bit of HTML, added some JS

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// Set variables 
+const start = document.querySelector("#start");
+const quiz = document.querySelector("#quiz");
+const question = document.querySelector("#question");
+const choiceA = document.querySelector("#A");
+const choiceB = document.querySelector("#B");
+const choiceC = document.querySelector("#C");
+
+
+
