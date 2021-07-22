d7bdfa95162c51e9716b90932559de3d04ab020c
Steph Heins <stephanie_heins@yahoo.com>
2021-07-22 13:49:55 -0700

    Added questions array

--- Assets/script.js
+++ Assets/script.js
@@ -6,5 +6,28 @@ const choiceA = document.querySelector("#A");
 const choiceB = document.querySelector("#B");
 const choiceC = document.querySelector("#C");
 
-
+//Questions Array 
+let questions = [
+    {
+        question: "Question 1",
+        choiceA: "Correct",
+        choiceB: "Wrong",
+        choiceC: "Wrong",
+        correct: "A"
+    },
+    {
+        question: "Question 2",
+        choiceA: "Wrong",
+        choiceB: "Correct",
+        choiceC: "Wrong",
+        correct: "B"
+    },
+    {
+        question: "Question 3",
+        choiceA: "Wrong",
+        choiceB: "Wrong",
+        choiceC: "Correct",
+        correct: "C"
+    }
+]
 
